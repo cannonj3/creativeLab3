3f827ab776d44e3e3a8484f85514eb215f0a7d29
Joseph Gillespie <jgilles3@kenai.cs.byu.edu>
2018-03-01 17:52:39 -0700

    Creative Lab 3 Finished

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ Vue.component('star-rating', VueStarRating.default);
 
 var app = new Vue({
   el: '.app',
-  data: {
-    page: '',
+  data:{
+    color: '#90EE90',
     addedName: '',
     addedComment: '',
-    comments: {},
+    comments1: [],
+    comments2: [],
+    comments3: [],
     rating: 0,
     totalRating: {},
     numberRatings: {},
@@ -14,12 +16,43 @@ var app = new Vue({
   },
   methods: {
 
-    addComment: function() {
-      if (!(this.page in this.comments))
-	       Vue.set(app.comments, this.page, new Array);
-	    this.comments[this.page].push({author:this.addedName,text:this.addedComment,time:Date()});
+    addComment1: function() {
+	    this.comments1.push({author:this.addedName,text:this.addedComment,date:this.currentDate()});
       this.addedName = '';
       this.addedComment = '';
+      this.color = 'transparent';
+    },
+    addComment2: function() {
+	    this.comments2.push({author:this.addedName,text:this.addedComment,date:this.currentDate()});
+      this.addedName = '';
+      this.addedComment = '';
+      this.color = 'transparent';
+    },
+    addComment3: function() {
+	    this.comments3.push({author:this.addedName,text:this.addedComment,date:this.currentDate()});
+      this.addedName = '';
+      this.addedComment = '';
+      this.color = 'transparent';
+    },
+    currentDate: function() {
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth()+1; //January is 0
+      var hour = today.getHours();
+      var minutes = today.getMinutes();
+
+      var yyyy = today.getFullYear();
+      if(dd<10){
+          dd='0'+dd;
+      }
+      if(mm<10){
+          mm='0'+mm;
+      }
+      if(minutes < 10){
+        minutes='0'+minutes;
+      }
+      var today = mm+'/'+dd+'/'+yyyy+ " " + hour + ":" + minutes;
+      return today;
     },
       /*addRating: function(rating) {
 
